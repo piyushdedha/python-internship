07a77d2e2cd2cd841a68ac996a98175f51c38f9b
piyushdedha <piyushdedhaa1111@gmail.com>
2025-12-30 19:44:26 +0530

    Create student_info.py

---
+++ student_info.py
@@ -0,0 +1,10 @@
+print("Student Information System")
+
+name = input("Enter student name: ")
+roll = input("Enter roll number: ")
+course = input("Enter course: ")
+
+print("\n--- Student Details ---")
+print("Name:", name)
+print("Roll No:", roll)
+print("Course:", course)
